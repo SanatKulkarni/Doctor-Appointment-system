57588282dd99a389a6f9905150e2ffc7fa46e05b
Sanat Kulkarni <87092449+SanatKulkarni@users.noreply.github.com>
2024-02-17 20:07:20 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getAnalytics } from "firebase/analytics";
-import {REACT_APP_API_KEY, REACT_APP_AUTH_DOMAIN, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID, REACT_APP_APP_ID, REACT_APP_MEASUREMENT_ID} from './constants/API_KEY';
+
 
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-  appId: REACT_APP_APP_ID,
-  measurementId: REACT_APP_MEASUREMENT_ID
+  apiKey: "1234",
+  authDomain: "1234",
+  projectId: "1234",
+  storageBucket: "1234",
+  messagingSenderId: "1234",
+  appId: "1234",
+  measurementId: "1234"
 };
 
 const app = initializeApp(firebaseConfig);
