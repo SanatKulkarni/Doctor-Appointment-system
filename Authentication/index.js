fe0750a41c9d783ff36ae2023571f11d6b9851f4
Sanat Kulkarni <87092449+SanatKulkarni@users.noreply.github.com>
2024-03-14 20:43:56 +0530

    Ported to Vanilla HTML/CSS/JS

---
+++ Authentication/index.js
@@ -0,0 +1,128 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCiccwJxhwyelAmdSMO5x6F2KHFo1dGG_A",
+    authDomain: "video-chat-app-a6454.firebaseapp.com",
+    projectId: "video-chat-app-a6454",
+    storageBucket: "video-chat-app-a6454.appspot.com",
+    messagingSenderId: "604116491705",
+    appId: "1:604116491705:web:038d684738df7b81882a68",
+    databaseURL: "https://video-chat-app-a6454-default-rtdb.asia-southeast1.firebasedatabase.app" 
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth()
+  const database = firebase.database()
+
+  function register () {
+
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+    full_name = document.getElementById('full_name').value
+    favourite_song = document.getElementById('favourite_song').value
+    milk_before_cereal = document.getElementById('milk_before_cereal').value
+
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is Outta Line!!')
+      return
+
+    }
+    if (validate_field(full_name) == false || validate_field(favourite_song) == false || validate_field(milk_before_cereal) == false) {
+      alert('One or More Extra Fields is Outta Line!!')
+      return
+    }
+
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(function() {
+
+      var user = auth.currentUser
+
+      var database_ref = database.ref()
+
+      var user_data = {
+        email : email,
+        full_name : full_name,
+        favourite_song : favourite_song,
+        milk_before_cereal : milk_before_cereal,
+        last_login : Date.now()
+      }
+
+      database_ref.child('users/' + user.uid).set(user_data)
+
+      alert('User Created!!')
+    })
+    .catch(function(error) {
+
+      var error_code = error.code
+      var error_message = error.message
+
+      alert(error_message)
+    })
+  }
+
+  function login () {
+
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is Outta Line!!')
+      return
+    }
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+
+      var user = auth.currentUser
+
+      var database_ref = database.ref()
+
+      // Fetch existing user data
+      database_ref.child('users/' + user.uid).once('value').then(function(snapshot) {
+        var user_data = snapshot.val();
+
+        // Update last_login field
+        user_data.last_login = Date.now();
+
+        // Update user data in the database
+        database_ref.child('users/' + user.uid).update(user_data)
+
+        alert('User Logged In!!')
+      });
+    })
+    .catch(function(error) {
+
+      var error_code = error.code
+      var error_message = error.message
+
+      alert(error_message)
+    })
+}
+
+
+  function validate_email(email) {
+    expression = /^[^@]+@\w+(\.\w+)+\w$/
+    if (expression.test(email) == true) {
+
+      return true
+    } else {
+
+      return false
+    }
+  }
+
+  function validate_password(password) {
+
+    if (password < 6) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  function validate_field(field) {
+    if (field == null) {
+        return false;
+    } else {
+        return true;
+    }
+}
